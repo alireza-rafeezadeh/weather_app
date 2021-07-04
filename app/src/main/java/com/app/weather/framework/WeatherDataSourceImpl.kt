3c0b6fb657d5058f6faf2bc4fb04643c917b3446
Alireza Rafeezadeh <rafeezadeh.alireza@gmail.com>
2021-07-04 10:50:52 +0430

    WEAT-4 Add weather data source implementation

--- app/src/main/java/com/app/weather/framework/WeatherDataSourceImpl.kt
+++ app/src/main/java/com/app/weather/framework/WeatherDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.app.weather.framework
 import com.app.core.data.datasource.WeatherDataSource
 import com.app.core.data.network.api.weather.WeatherApi
 import com.app.core.data.repository.BaseDataSource
+import com.app.core.data.repository.ResultWrapper
 import com.app.core.domain.ForecastResponse
 import kotlinx.coroutines.flow.Flow
 import retrofit2.Response
@@ -12,20 +13,12 @@ import javax.inject.Inject
 class WeatherDataSourceImpl @Inject constructor(private val weatherApi: WeatherApi) :
     BaseDataSource(), WeatherDataSource{
 
-    override suspend fun forecast() : Flow<ForecastResponse> {
+    override suspend fun forecast() : Flow<ResultWrapper<ForecastResponse>> {
 //        weatherApi.forecast("","","",1)
-
         return flowOnIO {
             weatherApi.forecast("","","",14)
         }
 
     }
 
-
-   suspend fun sdfsf(): Flow<ForecastResponse> {
-
-        return flowOnIO {
-            weatherApi.forecast("","","",14)
-        }
-    }
 }
