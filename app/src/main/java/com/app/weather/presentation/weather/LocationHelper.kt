4bb2d6757369cc2a3ee0b254cca072ea449d4541
Alireza Rafeezadeh <rafeezadeh.alireza@gmail.com>
2021-07-07 10:09:40 +0430

    WEAT-5 #progress Add location helper

---
+++ app/src/main/java/com/app/weather/presentation/weather/LocationHelper.kt
@@ -0,0 +1,90 @@
+package com.app.weather.presentation.weather
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.LocationServices
+
+
+class LocationHelper {
+
+    fun checkLocationPermission(
+        fragment: Fragment,
+        isGrantedAction: (latLong : String) -> Unit,
+        featureUnavailableAction: () -> Unit
+    ) {
+        val requestPermissionLauncher =
+            fragment.registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                    getLatLong(fragment, isGrantedAction)
+                } else {
+                    featureUnavailableAction()
+                }
+            }
+
+        when {
+            ContextCompat.checkSelfPermission(
+                fragment.requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                getLatLong(fragment, isGrantedAction)
+            }
+            /*shouldShowRequestPermissionRationale(...) -> {
+            // In an educational UI, explain to the user why your app requires this
+            // permission for a specific feature to behave as expected. In this UI,
+            // include a "cancel" or "no thanks" button that allows the user to
+            // continue using your app without granting the permission.
+            showInContextUI(...)
+        }*/
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+        }
+    }
+
+
+    private fun getLatLong(fragment: Fragment, isGrantedAction : (latLong : String) -> Unit ) {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(fragment.requireActivity())
+
+
+        if (ActivityCompat.checkSelfPermission(
+                fragment.requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                fragment.requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Got last known location. In some rare situations this can be null.
+                Log.i("location_tag", "performAction: -> $location ")
+
+                Log.i("lat_long_tag", "${location?.latitude}/${location?.longitude}")
+
+                location?.getLatLong()?.let {
+                    isGrantedAction(it)
+                }
+            }
+    }
+}
